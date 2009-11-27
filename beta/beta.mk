6bf1c5159860c03e86f5b9ba197ebd17d7b3909f
Juergen Schulze <juergens@met.no>
2009-11-27 12:10:23 +0000

    added beta.mk

---
+++ beta/beta.mk
@@ -0,0 +1,8 @@
+
+applicationsdir        = $(datarootdir)/applications
+dist_applications_DATA = \
+	beta/metno-tseriesTEST.desktop
+
+controlfiledir = $(sysconfdir)/tseriesTEST
+dist_controlfile_DATA= \
+	beta/tseries.ctl
